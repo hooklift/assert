7fc55c20bd19a10e442247acc69a64eea8a4886c
Camilo Aguilar <camilo.aguilar@gmail.com>
2015-02-10 10:45:26 -0500

    Adds documentation

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # Minimalistic assertion library
+
+## Installation
+go get -u github.com/hooklift/assert
+
+### Usage
+
+```go
+func TestSave(t *testing.T) {
+	u := &Account{
+		repo:     &RepoMock{},
+		Email:    "camilo@hooklift.io",
+		Password: "mypassword",
+		Name:     "Camilo Aguilar",
+	}
+
+	err := u.Save()
+	assert.Ok(t, err)
+	assert.Cond(t, u.ID != "", "User ID should not be empty: %v", u)
+
+	u2, err := Get(u.ID)
+	assert.Ok(t, err)
+	assert.Equals(t, u.ID, u2.ID)
+}
+```
--- assert.go
+++ assert.go
@@ -1,4 +1,4 @@
-package assertion
+package assert
 
 import (
 	"fmt"
@@ -6,17 +6,10 @@ import (
 	"reflect"
 	"runtime"
 	"testing"
-
-	log "github.com/Sirupsen/logrus"
 )
 
-func init() {
-	//log.SetOutput(ioutil.Discard)
-	log.SetLevel(log.DebugLevel)
-}
-
 // assert fails the test if the condition is false.
-func assert(tb testing.TB, condition bool, msg string, v ...interface{}) {
+func Cond(tb testing.TB, condition bool, msg string, v ...interface{}) {
 	if !condition {
 		_, file, line, _ := runtime.Caller(1)
 		fmt.Printf("\033[31m%s:%d: "+msg+"\033[39m\n\n", append([]interface{}{filepath.Base(file), line}, v...)...)
@@ -25,7 +18,7 @@ func assert(tb testing.TB, condition bool, msg string, v ...interface{}) {
 }
 
 // ok fails the test if an err is not nil.
-func ok(tb testing.TB, err error) {
+func Ok(tb testing.TB, err error) {
 	if err != nil {
 		_, file, line, _ := runtime.Caller(1)
 		fmt.Printf("\033[31m%s:%d: unexpected error: %s\033[39m\n\n", filepath.Base(file), line, err.Error())
@@ -34,7 +27,7 @@ func ok(tb testing.TB, err error) {
 }
 
 // equals fails the test if exp is not equal to act.
-func equals(tb testing.TB, exp, act interface{}) {
+func Equals(tb testing.TB, exp, act interface{}) {
 	if !reflect.DeepEqual(exp, act) {
 		_, file, line, _ := runtime.Caller(1)
 		fmt.Printf("\033[31m%s:%d:\n\n\texp: %#v\n\n\tgot: %#v\033[39m\n\n", filepath.Base(file), line, exp, act)
